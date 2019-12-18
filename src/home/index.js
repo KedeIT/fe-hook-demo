e240a224057368b70b5b1aba5cbfb5f11febbc13
Clare <lilingjie1992@gmail.com>
2019-12-19 07:56:28 +0800

    更改

--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import InfoMarkdown from '../Info.md';
 function List(props) {
     const { title, data } = props;
     return (
@@ -17,6 +18,17 @@ function List(props) {
 }
 
 function Home(props) {
+    const [input, setInput] = useState('');
+
+    useEffect(() => {
+        async function loadMarkdown () {
+            const result = await fetch(InfoMarkdown);
+            const text = await result.text();
+            setInput(text);
+        }
+        loadMarkdown();
+    }, []);
+
     const infos = [
         '简介',
         '优势',
@@ -33,12 +45,9 @@ function Home(props) {
         "现在很多常用的第三方库已支持Hook，比如react-router-dom、react-redux、mobx-react。"
     ];
     const hookApis = [
-        'useState',
-        'useEffect',
-        'useContext',
-        'useReducer',
-        'useCallback',
-        'useMemo',
+        'useReducer -- useState的替代品，适用于复杂的state',
+        'useCallback -- 返回一个memorized函数',
+        'useMemo -- 返回一个memorized值',
         'useRef -- 返回一个可变ref对象',
         'useImperativeHandle -- 可以让你在使用 ref 时自定义暴露给父组件的实例值',
         'useLayoutEffect -- 和useEffect作用相同，但是是在DOM变更后同步触发重渲染',
@@ -52,12 +61,20 @@ function Home(props) {
 
     return (
         <div>
-            <p>Hook是React16.8之后推出的新特性，可以让你在不编写class的情况下使用state及其它class特性。</p>
-            <p>介绍</p>
+            <p>Hook是React16.8的新增特性，可以让你在不编写class的情况下使用state及其它React特性。</p>
             <p>优势</p>
+            <p>1.Hook使你在无需修改组件结构的情况下复用状态逻辑</p>
+            <p>2.Hook让组件变得更加容易理解</p>
+            <p></p>
+            <p></p>
             <List title="趋势" data={trends} />
-            <List title="Hook Api" data={hookApis} />
+            <p>常用API</p>
+            <p>useState</p>
+            <p>useEffect</p>
+            <p>useContext</p>
+            <List title="不常用API" data={hookApis} />
             <List title="使用规则" data={rules} />
+            <ReactMarkdown source={input} />
         </div>
     );
 }
